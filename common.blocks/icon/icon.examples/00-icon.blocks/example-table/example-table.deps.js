e92afaa9b3a4974456f6f52b8e60b196088cf03b
alios <abc.ua@yandex.ru>
2013-08-27 20:17:17 +0300

    icon initial

---
+++ common.blocks/icon/icon.examples/00-icon.blocks/example-table/example-table.deps.js
@@ -0,0 +1,20 @@
+({
+    shouldDeps: [
+        {
+            block: 'radio-button',
+            mods: {
+                color: ['transparent'],
+                size: ['s']
+            }
+        },
+        {
+            block: 'icon',
+            mods: {
+                hovered: ['yes'],
+                pressed: ['yes'],
+                type: ['info', 'refresh', 'dashboard', 'timeline', 'description', 'arrow-up', 'apps-info', 'like', 'dislike', 'monkey', 'thin-arrow-left', 'thin-arrow-right'],
+                size: ['s', 'm', 'l', 'xl', 'xxl']
+            }
+        }
+    ]
+})
