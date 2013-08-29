cf42b80decf300da0c685623821b3389ce9addec
aliosv <abc.ua@yandex.ru>
2013-08-29 18:17:28 -0400

    update ci config

--- ci-build.sh
+++ ci-build.sh
@@ -1,7 +1,10 @@
-rm -rf release desktop.sets/*
 npm i
 ./node_modules/bem/bin/bem make libs
-./node_modules/bem/bin/bem make sets
 ./node_modules/bem/bin/bem make site
+mv release temp
 git checkout gh-pages
+rm -rf release
+mv temp release
+git add .
+git commit -m "update site, build #$BUILD_NUMBER"
 git push origin gh-pages
