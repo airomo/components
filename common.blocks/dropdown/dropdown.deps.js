6a959a276d5f4b3338d72a0fcf967ba22291186f
alios <abc.ua@yandex.ru>
2013-09-02 01:38:08 +0300

    refactoring, update documentation, create dropdown_align_center.js

---
+++ common.blocks/dropdown/dropdown.deps.js
@@ -0,0 +1,11 @@
+({
+    shouldDeps: [
+        {
+            mods: {
+                align: ['start', 'center', 'end'],
+                direction: ['top', 'right', 'bottom', 'left']
+            },
+            elems: ['switcher', {elem: 'content', mods: {visibility: 'hidden'}}, 'item']
+        }
+    ]
+})
--- common.blocks/dropdown/dropdown.js
+++ common.blocks/dropdown/dropdown.js
@@ -5,27 +5,27 @@ BEM.DOM.decl('dropdown', {
                 var _this = this,
                     isClicked;
 
-                //toggle block visibility when elem 'switcher' was clicked
+                //toggle __content visibility when elem 'switcher' was clicked
                 this.bindTo(this.elem('switcher'), 'click', function(e) {
                     isClicked = true;
 
-                    _this.toggleMod('visibility', 'hidden', '');
+                    _this.toggleMod(_this.elem('content-wrapper'), 'visibility', 'hidden', '');
                 });
 
                 //hide dropdown when elem 'item' was clicked
                 this.bindTo(_this.elem('item'), 'click', function(e) {
-                    _this.setMod('visibility', 'hidden');
+                    _this.setMod(_this.elem('content-wrapper'), 'visibility', 'hidden');
                 });
 
                 //prevent click bubling on dropdown content
-                this.bindTo(this.elem('content'), 'click', function(e) {
+                this.bindTo(this.elem('content-wrapper'), 'click', function(e) {
                     e.preventDefault();
                     return false;
                 });
 
                 //hide dropdown if clicking out of content
                 this.bindToDoc('click', function(e) {
-                    !isClicked && _this.setMod('visibility', 'hidden');
+                    !isClicked && _this.setMod(_this.elem('content-wrapper'), 'visibility', 'hidden');
 
                     isClicked = false;
                 });
