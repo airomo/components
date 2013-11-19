bfd0a0ed79a9506d59e8e46cadc9f68e0ac8a3fb
alios <abc.ua@yandex.ru>
2013-11-20 01:35:17 +0200

    build script update

--- build-prod.sh
+++ build-prod.sh
@@ -61,7 +61,14 @@ if [ `echo $GIT_BRANCH | sed 's/origin\///'` != 'gh-pages' ]; then
     git commit -m "update site, build #$BUILD_NUMBER"
     git push origin +gh-pages
 
+<<<<<<< HEAD
     #back on start branch
     git checkout $GIT_BRANCH
 
 fi
+=======
+    #back on checkouted branch
+    git checkout $GIT_BRANCH
+
+fi
+>>>>>>> build script update
